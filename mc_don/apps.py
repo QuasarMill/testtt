0f596e26d940431ec5714a391fe5a1b06f94a64f
QuasarMill <kudadmitry123@gmail.com>
2024-10-30 16:54:23 +0300

    Add files via upload

---
+++ mc_don/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class McDonConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mc_don'
---
+++ simpleapp/templatetags/custom_filters.py
@@ -0,0 +1,22 @@
+from django import template
+
+
+register = template.Library()
+
+
+CURRENCIES_SYMBOLS = {
+   'rub': 'Р',
+   'usd': '$',
+   'eur': '€'
+}
+
+
+@register.filter()
+def currency(value, code='rub'):
+   """
+   value: значение, к которому нужно применить фильтр
+   code: код валюты
+   """
+   postfix = CURRENCIES_SYMBOLS[code]
+
+   return f'{value} {postfix}'
